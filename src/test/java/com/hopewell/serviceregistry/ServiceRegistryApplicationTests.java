ef6cfbc86b707b2a712c5ba85eb4387aab7dc175
Simphiwe Mthanti <simphiwe@dots.africa>
2023-08-16 22:12:11 +0200

    Spring Boot 3 eureka-server registry

---
+++ src/test/java/com/hopewell/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopewell.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
